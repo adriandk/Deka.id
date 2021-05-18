cd4b5c930826a0d37b1d09d96c64732522c2fe5e
adriandk <adriandaniel1803@gmail.com>
2021-05-18 15:13:22 +0700

    Add some Jetpack library

--- app/src/main/java/com/adrian/dekaid/data/FilmData.kt
+++ app/src/main/java/com/adrian/dekaid/data/FilmData.kt
@@ -1,8 +1,13 @@
 package com.adrian.dekaid.data
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FilmData(
+    val movieId: String,
+    val movieImage: Int,
+    val movieTitle: String,
+    val movieDescription: String,
+    val movieReleaseDate: String
 ) : Parcelable
--- app/src/main/java/com/adrian/dekaid/ui/splash/MainActivity.kt
+++ app/src/main/java/com/adrian/dekaid/ui/splash/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.adrian.dekaid.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.adrian.dekaid.R
+import com.adrian.dekaid.ui.home.HomeActivity
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2500)
+
     }
 }
