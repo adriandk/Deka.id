3c58dfab5c32f8eaecfef478570cde7ab5193368
adriandk <adriandaniel1803@gmail.com>
2021-05-20 19:44:03 +0700

    add ViewModel

--- app/src/main/java/com/adrian/dekaid/ui/home/HomeActivity.kt
+++ app/src/main/java/com/adrian/dekaid/ui/home/HomeActivity.kt
@@ -1,12 +1,45 @@
 package com.adrian.dekaid.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.adrian.dekaid.R
+import com.adrian.dekaid.ui.movie.MovieFragment
+import com.adrian.dekaid.ui.tvshow.ShowFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private val movieFragment = MovieFragment()
+    private val tvShowFragment = ShowFragment()
+    private val fragmentManager = supportFragmentManager
+    var fragment: Fragment = movieFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        fragmentManager.beginTransaction().add(R.id.frame_layout, tvShowFragment)
+            .hide(tvShowFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.frame_layout, movieFragment).commit()
+
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.movie -> {
+                    fragmentManager.beginTransaction().hide(fragment).show(movieFragment).commit()
+                    fragment = tvShowFragment
+                }
+                R.id.tvshow -> {
+                    fragmentManager.beginTransaction().hide(fragment).show(tvShowFragment).commit()
+                    fragment = movieFragment
+                }
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
