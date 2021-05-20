d9de30efe37c09f3c28094fc41fba4028c4bfce1
adriandk <adriandaniel1803@gmail.com>
2021-05-20 10:44:43 +0700

    Working on design

--- app/src/main/java/com/adrian/dekaid/data/MovieData.kt
+++ app/src/main/java/com/adrian/dekaid/data/MovieData.kt
@@ -4,10 +4,12 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class FilmData(
+data class MovieData(
     val movieId: String,
-    val movieImage: Int,
     val movieTitle: String,
+    val movieDuration: String,
+    val movieGenre: String,
     val movieDescription: String,
-    val movieReleaseDate: String
+    val movieReleaseYear: String,
+    val movieImage: Int,
 ) : Parcelable
--- app/src/main/java/com/adrian/dekaid/utils/DataDummy.kt
+++ app/src/main/java/com/adrian/dekaid/utils/DataDummy.kt
@@ -1,4 +1,198 @@
 package com.adrian.dekaid.utils
 
+import com.adrian.dekaid.R
+import com.adrian.dekaid.data.MovieData
+
+
 object DataDummy {
+
+    fun getMovieData(): ArrayList<MovieData> {
+        return arrayListOf(
+            MovieData(
+                "m1",
+                "Aquaman",
+                "143 min",
+                "Action",
+                "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orm's half-human, half-Atlantean brother and true heir to the throne.",
+                "2018",
+                R.drawable.poster_aquaman
+            ),
+            MovieData(
+                "m2",
+                "Avengers: Infinity War",
+                "149 min",
+                "Adventure",
+                "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+                "2018",
+                R.drawable.poster_infinity_war
+            ),
+            MovieData(
+                "m3",
+                "How To Train Your Dragon: The Hidden World",
+                "104 min",
+                "Adventure",
+                "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.",
+                "2019",
+                R.drawable.poster_how_to_train
+            ),
+            MovieData(
+                "m4",
+                "Spiderman: Into Spider Verse",
+                "117 min",
+                "Action",
+                "Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson \"Kingpin\" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.",
+                "2019",
+                R.drawable.poster_spiderman
+            ),
+            MovieData(
+                "m5",
+                "Creed II",
+                "130 min",
+                "Drama",
+                "Between personal obligations and training for his next big fight against an opponent with ties to his family's past, Adonis Creed is up against the challenge of his life.",
+                "2018",
+                R.drawable.poster_creed
+            ),
+            MovieData(
+                "m6",
+                "A Star Is Born",
+                "136 min",
+                "Drama",
+                "Seasoned musician Jackson Maine discovers — and falls in love with — struggling artist Ally. She has just about given up on her dream to make it big as a singer — until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+                "2018",
+                R.drawable.poster_a_start_is_born
+            ),
+            MovieData(
+                "m7",
+                "Alita: Battle Angel",
+                "122 min",
+                "Sci-Fi",
+                "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
+                "2019",
+                R.drawable.poster_alita
+            ),
+            MovieData(
+                "m8",
+                "Bohemian Rhapsody",
+                "135 min",
+                "Music",
+                "Singer Freddie Mercury, guitarist Brian May, drummer Roger Taylor and bass guitarist John Deacon take the music world by storm when they form the rock 'n' roll band Queen in 1970. Hit songs become instant classics. When Mercury's increasingly wild lifestyle starts to spiral out of control, Queen soon faces its greatest challenge yet – finding a way to keep the band together amid the success and excess.",
+                "2018",
+                R.drawable.poster_bohemian
+            ),
+            MovieData(
+                "m9",
+                "Ralph Breaks The Internet",
+                "112 min",
+                "Animation",
+                "Video game bad guy Ralph and fellow misfit Vanellope von Schweetz must risk it all by traveling to the World Wide Web in search of a replacement part to save Vanellope's video game, Sugar Rush. In way over their heads, Ralph and Vanellope rely on the citizens of the internet — the netizens — to help navigate their way, including an entrepreneur named Yesss, who is the head algorithm and the heart and soul of trend-making site BuzzzTube.",
+                "2018",
+                R.drawable.poster_ralph
+            ),
+            MovieData(
+                "m10",
+                "Fantastic Beasts: The Crimes of Grindelwald",
+                "134 min",
+                "Adventure",
+                "Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause—elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.",
+                "2018",
+                R.drawable.poster_crimes
+            ),
+        )
+    }
+
+    fun getShowData(): ArrayList<MovieData> {
+        return arrayListOf(
+            MovieData(
+                "tv1",
+                "Arrow",
+                "8 Seasons",
+                "Crime",
+                "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
+                "2012",
+                R.drawable.poster_arrow
+            ),
+            MovieData(
+                "tv2",
+                "Family Guy",
+                "19 Seasons",
+                "Comedy",
+                "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+                "1999",
+                R.drawable.poster_family_guy
+            ),
+            MovieData(
+                "tv3",
+                "The Flash",
+                "7 Seasons",
+                "Sci-Fi",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "2014",
+                R.drawable.poster_flash
+            ),
+            MovieData(
+                "tv4",
+                "Game Of Thrones",
+                "8 Seasons",
+                "Adventure",
+                "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+                "2011",
+                R.drawable.poster_god
+            ),
+            MovieData(
+                "tv5",
+                "Gotham",
+                "5 Seasons",
+                "Drama",
+                "Everyone knows the name Commissioner Gordon. He is one of the crime world's greatest foes, a man whose reputation is synonymous with law and order. But what is known of Gordon's story and his rise from rookie detective to Police Commissioner? What did it take to navigate the multiple layers of corruption that secretly ruled Gotham City, the spawning ground of the world's most iconic villains? And what circumstances created them – the larger-than-life personas who would become Catwoman, The Penguin, The Riddler, Two-Face and The Joker?",
+                "2014",
+                R.drawable.poster_gotham
+            ),
+            MovieData(
+                "tv6",
+                "Naruto Shippūden",
+                "20 Seasons",
+                "Anime",
+                "Naruto Shippuuden is the continuation of the original animated TV series Naruto.The story revolves around an older and slightly more matured Uzumaki Naruto and his quest to save his friend Uchiha Sasuke from the grips of the snake-like Shinobi, Orochimaru. After 2 and a half years Naruto finally returns to his village of Konoha, and sets about putting his ambitions to work, though it will not be easy, as He has amassed a few (more dangerous) enemies, in the likes of the shinobi organization; Akatsuki.",
+                "2007",
+                R.drawable.poster_naruto_shipudden
+            ),
+            MovieData(
+                "tv7",
+                "Supergirl",
+                "6 Seasons",
+                "Sci-Fi",
+                "Twenty-four-year-old Kara Zor-El, who was taken in by the Danvers family when she was 13 after being sent away from Krypton, must learn to embrace her powers after previously hiding them. The Danvers teach her to be careful with her powers, until she has to reveal them during an unexpected disaster, setting her on her journey of heroism.",
+                "2015",
+                R.drawable.poster_supergirl
+            ),
+            MovieData(
+                "tv8",
+                "The Simpsons",
+                "32 Seasons",
+                "Animation",
+                "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
+                "1989",
+                R.drawable.poster_the_simpson
+            ),
+            MovieData(
+                "tv9",
+                "The Umbrella Academy",
+                "2 Seasons",
+                "Action",
+                "A dysfunctional family of superheroes comes together to solve the mystery of their father's death, the threat of the apocalypse and more.",
+                "2019",
+                R.drawable.poster_the_umbrella
+            ),
+            MovieData(
+                "tv10",
+                "The Walking Dead",
+                "10 Seasons",
+                "Adventure",
+                "Sheriff's deputy Rick Grimes awakens from a coma to find a post-apocalyptic world dominated by flesh-eating zombies. He sets out to find his family and encounters many other survivors along the way.",
+                "2010",
+                R.drawable.poster_the_walking_dead
+            ),
+        )
+    }
 }
